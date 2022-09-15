bc7542ffc23e1862cf19d2ecd9c1664c0fd238e2
Subham Mandal <subhammandal01@gmail.com>
2022-09-15 22:56:04 +0530

    Create config.sh

---
+++ vagrantfiles/config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function sudo_conf() {
+cat > /etc/sudoers.d/ansible << EOF
+%ansible  ALL=(ALL) NOPASSWD:ALL
+EOF
+}
+
+function add_user() {
+case $(egrep '^(NAME)=' /etc/os-release|cut -d'=' -f2|sed 's|"||g') in
+"CentOS Linux")
+sudo yum install openssh-server openssl -y
+sudo service sshd start
+sudo useradd -p $(openssl passwd -1 ansible) ansible
+sudo_conf
+;;
+*)
+sudo useradd -p $(openssl passwd -1 ansible) ansible
+sudo sed -i "/^PasswordAuthentication/s; .*$; yes;" /etc/ssh/sshd_config
+sudo service sshd restart
+sudo_conf
+;;
+esac
+}
+
+
+add_user
