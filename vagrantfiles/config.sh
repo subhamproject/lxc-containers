46a7fd5296fccf385ea01a781547133f219ca59c
Subham Mandal <subhammandal01@gmail.com>
2022-09-16 01:30:15 +0530

    Update config.sh

--- vagrantfiles/config.sh
+++ vagrantfiles/config.sh
@@ -11,11 +11,11 @@ case $(egrep '^(NAME)=' /etc/os-release|cut -d'=' -f2|sed 's|"||g') in
 "CentOS Linux")
 sudo yum install openssh-server openssl -y
 sudo service sshd start
-sudo useradd -p $(openssl passwd -1 ansible) ansible
+sudo useradd -p $(openssl passwd -1 password) -m -d /home/ansible -s /bin/bash ansible
 sudo_conf
 ;;
 *)
-sudo useradd -p $(openssl passwd -1 ansible) ansible
+sudo useradd -p $(openssl passwd -1 password) -m -d /home/ansible -s /bin/bash ansible
 sudo sed -i "/^PasswordAuthentication/s; .*$; yes;" /etc/ssh/sshd_config
 sudo service sshd restart
 sudo_conf
