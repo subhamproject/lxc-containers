17210f7785ab7bc2e6ad8caf0ae866b7fca575a5
Subham Mandal <subhammandal01@gmail.com>
2022-09-17 22:55:11 +0530

    Update lxc_ssh_from_outside.sh

--- lxc_ssh_from_outside.sh
+++ lxc_ssh_from_outside.sh
@@ -19,5 +19,16 @@ root@ansible:~#
 
 Run below command to port forward from host to lxc container
 
-lxc config device add ansible-client-1 myport22 proxy listen=tcp:0.0.0.0:2222 connect=tcp:127.0.0.1:22
+lxc config device add ansible-client-1 myport22 proxy listen=tcp:0.0.0.0:2222(HOSTPORT) connect=tcp:127.0.0.1:22(CONTAINER PORT)
 
+
+
+Create profile and add to lxc containers
+
+#!/bin/bash
+
+lxc profile create proxy-22
+
+lxc profile device add proxy-22 hostport2000 proxy connect="tcp:127.0.0.1:22" listen="tcp:0.0.0.0:2000"
+
+for I in $(lxc list -c ns --format csv|cut -d, -f1); do lxc profile add $I proxy-22; done
